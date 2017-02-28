9a4d0f3cf34e7ab65e1f406daef3ef6f83e422f7
David Ogilo <david.ogilo@dayesoft.co.uk>
2017-02-28 19:30:25 +0100

    Worked on Theme CSS rules

--- src/WebSchema/Models/AMP/Rules/WP/Theme.php
+++ src/WebSchema/Models/AMP/Rules/WP/Theme.php
@@ -12,10 +12,56 @@ use WebSchema\Models\AMP\Rules\Model;
 
 class Theme extends Model
 {
-    const CSS_SIZE_LIMIT = 5000; //bytes
+    const CSS_SIZE_LIMIT = 50000; //50K
+
+    private static $rules = [
+        'applyImportantRule',
+        'applyMediaRule',
+        'applyWildCardRule'
+    ];
 
     private $stylePath = STYLESHEETPATH . '/style.css';
     private $css;
+
+    /**
+     * @param $css
+     * @return string
+     * @link https://www.ampproject.org/docs/guides/responsive/style_pages#disallowed-styles
+     */
+    public static function applyImportantRule($css)
+    {
+        return preg_replace('/[\w-]+?:(?: ?)[\w .%-()#]+ !important;/', '', $css);
+    }
+
+    /**
+     * @param $css
+     * @return string
+     * @link http://stackoverflow.com/questions/22312313/regex-parsing-of-css-media-queries-and-other-nested-selectors
+     */
+    public static function applyMediaRule($css)
+    {
+        return preg_replace('/@media print[^{]+\{([\s\S]+?})\s*}/', '', $css);
+    }
+
+    /**
+     * @param $css
+     * @return string
+     * @link https://www.ampproject.org/docs/guides/responsive/style_pages#disallowed-styles
+     */
+    public static function applyWildCardRule($css)
+    {
+        //remove all wildcard separated selectors
+        preg_match_all('/((?:,\n)?[\S ]*[^\/]\*[\S ]*[, ])/', $css, $matches);
+
+        foreach($matches[0] as $match)
+        {
+            if (strpos($match, ',') !== false) {
+               $css = str_replace($match, '', $css);
+            }
+        }
+
+        return preg_replace('/((?:[\S ]*)\*(?!=)[^}\/]+})/', '', $css);
+    }
 
     public function parse()
     {
@@ -29,12 +75,26 @@ class Theme extends Model
      */
     public static function cleanCSS($css)
     {
-        $css = preg_replace(['/\/\*.+?\*\//s', '/[\s]+/'], '', $css);
+        foreach (self::$rules as $rule) {
+            $css = self::$rule($css);
+        }
+
+        $regex = [
+            '/\/\*[\s\S]+?\*\//s'  => '', //remove comments
+            '/[\t\r\n\v\f]+/' => '', //remove single space character
+            '/(:) | ({)/'     => '$1$2', //remove spaces between : and {
+        ];
+
+        $css = preg_replace(array_keys($regex), array_values($regex), $css);
+
         return $css;
     }
 
     private function addCSS()
     {
+//        echo strlen($this->css);
+//        exit;
+
         if (strlen($this->css) <= self::CSS_SIZE_LIMIT) {
             $style = $this->document->createElement('style');
             $style->setAttribute('amp-custom', '');
